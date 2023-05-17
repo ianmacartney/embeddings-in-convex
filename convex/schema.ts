b357c9730e367b118f1815946f83395c5b79621a
Seth Raphael <git@magicseth.com>
2023-05-17 10:38:04 -0700

    add schema

---
+++ convex/schema.ts
@@ -0,0 +1,28 @@
+import { v } from "convex/values";
+import { defineSchema, defineTable } from "convex/schema";
+
+const document = v.object({});
+const excerpt = v.object({
+  document_id: v.id("documents"),
+  text: v.string(),
+  index: v.number(),
+});
+
+defineSchema({
+  documents: defineTable(document),
+  excerpts: defineTable(excerpt),
+});
+
+// const llmMessage = v.object({
+//     a:v.optional(v.string()),
+//     u:v.optional(v.string()),
+//     s:v.optional(v.string()),
+//     name:v.optional(v.string()),
+//   })
+
+// const llmChatRequest = v.object({
+//     promptMessages : v.array(llmMessage),
+//     systemPrompt: v.optional(v.string()),
+//     model: v.optional(v.string()),
+//     max_tokens: v.optional(v.number()),
+//   })
