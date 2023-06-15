82519952e768cc5d6e337eac0dd398211cacbcfd
Ian Macartney <ian@convex.dev>
2023-06-15 16:56:31 -0400

    rewind-ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,jsx,tsx}",
+    // you can either add all styles
+    "./node_modules/@rewind-ui/core/dist/theme/styles/*.js",
+    // OR you can add only the styles you need
+    // "./node_modules/@rewind-ui/core/dist/theme/styles/Button.styles.js",
+    // "./node_modules/@rewind-ui/core/dist/theme/styles/Text.styles.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("tailwind-scrollbar")({ nocompatible: true }),
+    require("@tailwindcss/forms")({
+      strategy: "class", // only generate classes
+    }),
+  ],
 };
