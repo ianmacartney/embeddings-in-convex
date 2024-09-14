4e4bf844f1b4814e1337390f8a50f5d57b31a64f
Ian Macartney <ianhmacartney@gmail.com>
2024-09-13 21:03:59 -0700

    cjs -> mjs for eslint

--- .eslintrc.mjs
+++ .eslintrc.mjs
@@ -11,5 +11,6 @@ module.exports = {
   rules: {
     "react-refresh/only-export-components": "warn",
     "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-floating-promises": "error",
   },
 };
